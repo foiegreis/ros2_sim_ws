3d1f15c712a8b1e89413bcecf0a431e78fced5f0
foiegreis <greta.russi@gmail.com>
2024-05-22 00:32:05 +0900

    first commit

---
+++ build/ros2_sim_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/foiegreis/ros2_sim_ws/src/ros2_sim_description
+# Build directory: /home/foiegreis/ros2_sim_ws/build/ros2_sim_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
